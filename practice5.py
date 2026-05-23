76945711fee8f7629f4047f656dd4ae5ab8faed1
koushalya2702-collab <koushalya2702@gmail.com>
2026-05-23 21:35:14 +0530

    add practice5.py

---
+++ practice5.py
@@ -0,0 +1,50 @@
+#Matplotlib Bars
+
+import matplotlib.pyplot as plt
+import numpy as np
+x=np.array(["A", "B", "C", "D"])
+y=np.array([3, 8, 1, 0])
+plt.bar(x,y)
+plt.show()
+
+
+#Horizontal Bars
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y)
+plt.show()
+
+#Bar Color
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, color = "red")
+plt.show()
+
+#Bar Width
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.bar(x, y, width = 0.1)
+plt.show()
+
+#Bar Height
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = np.array(["A", "B", "C", "D"])
+y = np.array([3, 8, 1, 10])
+
+plt.barh(x, y, height = 0.1)
+plt.show()
